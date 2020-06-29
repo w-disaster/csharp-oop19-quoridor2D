c62a23d7f1a2805a63c944dc26affea9df1e2cf6
unknown <thomas.castellani@studio.unibo.it>
2020-06-29 18:49:05 +0200

    Added PlaceBarrier class (with interface) and a class to test

---
+++ UnitTest/CastellaniThomas/BarrierPlacerTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+using csharp_oop19_quoridor2D.CastellaniThomas;
+
+namespace UnitTest.CastellaniThomas
+{
+    public class BarrierPlacerTest
+    {
+        public BarrierPlacerTest()
+        {
+        }
+    }
+}
---
+++ csharp-oop19-quoridor2D/CastellaniThomas/BarrierPlacer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using csharp_oop19_quoridor2D.Fabri_Luca;
+
+namespace csharp_oop19_quoridor2D.CastellaniThomas
+{
+    public interface IBarrierPlacer
+    {
+        void PlaceBarrier(); //devo aggiungere i parametri
+    }
+
+    public class BarrierPlacer : IBarrierPlacer
+    {
+        //attributo model
+        //derivati di model se servono
+        //Coordinate barriera
+        //Orientamento barriera
+
+        public BarrierPlacer()
+        {
+            //inizializzo attributi
+        }
+
+        public void PlaceBarrier() 
+        {
+            //implementazione piazzamento
+        }
+
+        //vari metodi privati di controllo
+    }
+}
