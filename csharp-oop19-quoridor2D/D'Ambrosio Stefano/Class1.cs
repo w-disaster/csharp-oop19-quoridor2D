bf1f2da9ecbba7fd2ba5bfcd35cacb07b790c88e
Stefano <stefano.dambrosio4@studio.unibo.it>
2020-07-04 16:42:12 +0200

    Created class PowerUp

---
+++ csharp-oop19-quoridor2D/D'Ambrosio Stefano/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop19_quoridor2D.D_Ambrosio_Stefano
+{
+    class PowerUp
+    {
+    }
+}
