c9c88a11b265cd0da5c6be99302b632655f4a3f2
Stefano <stefano.dambrosio4@studio.unibo.it>
2020-07-05 17:07:52 +0200

    Created test class for powerUps

---
+++ UnitTest/D'Ambrosio Stefano/PowerUpTest.cs
@@ -0,0 +1,27 @@
+﻿using csharp_oop19_quoridor2D.D_Ambrosio_Stefano;
+using NUnit.Framework;
+
+namespace UnitTest.D_Ambrosio_Stefano
+{
+    class PowerUpTest
+    {
+        private IPowerUp plusOneBarrierTest;
+        private IPowerUp plusOneMoveTest;
+
+        public PowerUpTest()
+        {
+            this.plusOneBarrierTest = new PowerUp(Type.PlusOneBarrier);
+            this.plusOneMoveTest = new PowerUp(Type.PlusOneMove);
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(Type.PlusOneBarrier, plusOneBarrierTest.Type);
+            Assert.AreEqual(Type.PlusOneMove, plusOneMoveTest.Type);
+            Assert.IsTrue(plusOneBarrierTest.Coordinate.Second > 1);
+            Assert.IsTrue(plusOneBarrierTest.Coordinate.Second < 8);
+        }
+
+    }
+}
--- csharp-oop19-quoridor2D/D'Ambrosio Stefano/PowerUp.cs
+++ csharp-oop19-quoridor2D/D'Ambrosio Stefano/PowerUp.cs
@@ -1,4 +1,5 @@
 ﻿using csharp_oop19_quoridor2D.Fabri_Luca.Positioning;
+using csharp_oop19_quoridor2D.Fabri_Luca.RoundBarriers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,15 @@ namespace csharp_oop19_quoridor2D.D_Ambrosio_Stefano
     public class PowerUp : IPowerUp
     {
         private readonly Random random;
-        private Type Type { get; }
-        private Coordinate Coordinate { get; }
+        public Type Type { get; }
+        public Coordinate Coordinate { get; }
 
-        public PowerUp(Type type, int boardDimension)
+        public PowerUp(Type type)
         {
             this.random = new Random();
             this.Type = type;
-            this.Coordinate = new Coordinate(2 + random.Next(boardDimension - 4), 2 + random.Next(boardDimension - 4));
+            this.Coordinate = new Coordinate(2 + random.Next(RoundBarriers.BoardDimension - 4),
+                2 + random.Next(RoundBarriers.BoardDimension - 4));
         }
 
     }
