f0d3da41d9ed404d2eb03a5874bfabe3a88f164e
Stefano <stefano.dambrosio4@studio.unibo.it>
2020-07-08 15:16:56 +0200

    Created class RoundPowerUps and interface

--- UnitTest/D'Ambrosio Stefano/PowerUpTest.cs
+++ UnitTest/D'Ambrosio Stefano/PowerUpTest.cs
@@ -17,10 +17,14 @@ namespace UnitTest.D_Ambrosio_Stefano
         [Test]
         public void Test()
         {
+            // Check if powerUp type is correct
             Assert.AreEqual(Type.PlusOneBarrier, plusOneBarrierTest.Type);
             Assert.AreEqual(Type.PlusOneMove, plusOneMoveTest.Type);
+            // Check the coordinates (powerUps can't spawn in the first two rows of each side)
+            Assert.IsTrue(plusOneBarrierTest.Coordinate.First >= 0);
+            Assert.IsTrue(plusOneBarrierTest.Coordinate.First <= 8);
             Assert.IsTrue(plusOneBarrierTest.Coordinate.Second > 1);
-            Assert.IsTrue(plusOneBarrierTest.Coordinate.Second < 8);
+            Assert.IsTrue(plusOneBarrierTest.Coordinate.Second < 7);     
         }
 
     }
---
+++ csharp-oop19-quoridor2D/D'Ambrosio Stefano/RoundPowerUps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop19_quoridor2D.D_Ambrosio_Stefano
+{
+    class RoundPowerUps
+    {
+        public static readonly int BarrierPowerUpNumber = 3;
+        public static readonly int MovePowerUpNumber = 3;
+
+        private List<PowerUp> powerUpList;
+
+        public RoundPowerUps()
+        {
+            powerUpList = new List<PowerUp>;
+
+			for (int i = 0; i < BarrierPowerUpNumber; i++)
+			{
+				PowerUp p = new PowerUp(Type.PlusOneBarrier);
+				if (this.powerUpList.Any() && this.powerUpList.Contains(p))
+				{
+					i--;
+				}
+				else
+				{
+					this.powerUpList.Add(p);
+				}
+			}
+			for (int i = 0; i < MovePowerUpNumber; i++)
+			{
+				PowerUp p = new PowerUp(Type.PlusOneMove);
+				if (this.powerUpList.Any() && this.powerUpList.Contains(p))
+				{
+					i--;
+				}
+				else
+				{
+					this.powerUpList.Add(p);
+				}
+			}
+		}
+
+		public RoundPowerUps(List<PowerUp> powerUpList)
+		{
+			this.powerUpList = powerUpList;
+		}
+
+		public List<PowerUp> getPowerUpsAsList() {
+				return this.powerUpList;
+			}
+
+		public void remove(PowerUp p)
+			{
+				this.powerUpList.Remove(p);
+			}
+	}
+}
